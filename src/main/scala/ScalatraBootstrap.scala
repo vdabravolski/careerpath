b75227b5fc146dfda6b9795ab3b08af6ebcabc9b
Vadzim Dabravolski <vadzim_dabravolski@epam.com>
2016-10-03 14:02:20 -0400

    Extending API functionality with delete method

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -9,9 +9,11 @@ class ScalatraBootstrap extends LifeCycle {
     val mongoClient=MongoClient("10.6.198.231", 27017)
 
     val accountColl = mongoClient("test")("accounts")
+    val projectColl = mongoClient("test")("projects")
 
     //mounting multiple controllers
     context.mount(new CareerServlet(accountColl), "/*") // controller for HTML pages
-    context.mount(new CareerAPIServlet(accountColl),"/api/*") //controller for API
+    context.mount(new AccountAPIServlet(accountColl),"/api/accounts/*") //controller for Account API
+    context.mount(new ProjectAPIServlet(projectColl),"/api/projects/*") //controller for Project API
   }
 }
--- src/main/scala/com/github/vdabravolski/CareerpathAPI.scala
+++ src/main/scala/com/github/vdabravolski/CareerpathAPI.scala
@@ -1,16 +1,19 @@
 package com.github.vdabravolski
 
 import org.scalatra._
-// JSON-related libraries
 import org.json4s.{DefaultFormats, Formats}
-// JSON handling support from Scalatra
 import org.scalatra.json._
 import com.mongodb.casbah.Imports._
+import scala.collection.mutable.ListBuffer
+
+
+// TODO:
+//1.   extend number of data fiels of account entity. Right now only status and name is collected.
+//2. Generate page with API documentation using Swagger.
 
 
 // this is routes class
-
-class CareerAPIServlet(mongoColl : MongoCollection) extends ScalatraServlet with NativeJsonSupport {
+class AccountAPIServlet(mongoColl : MongoCollection) extends ScalatraServlet with NativeJsonSupport {
 
   // Sets up automatic case class to JSON output serialization
   protected implicit val jsonFormats: Formats = DefaultFormats
@@ -20,18 +23,23 @@ class CareerAPIServlet(mongoColl : MongoCollection) extends ScalatraServlet with
     contentType = formats("json")
   }
 
-  get("/accounts") {
+  get("/") {
+    //for { x <- mongoColl} yield Account(x.get("name"),x.get("status"))
+    val results=mongoColl.find()
+    val allAccounts= AccountsColl
+    for (x <- results){
+      allAccounts.all += convertDBObjectToAccount(x)
+    }
+    allAccounts.all
+  }
+
+  get("/:name") {
     val q = MongoDBObject("name" -> params("name"))
 
     mongoColl.findOne(q) match {
       case Some(x) => Account(mongoColl.findOne(q).get("name"),mongoColl.findOne(q).get("status"))
-      case None => APIMessage("account",params("name"),"not found")
+      case None => SuccessMessage("account",params("name"),"not found")
     }
-  }
-
-  get("/projects"){
-    //todo is coming
-    APIMessage("project","","fetching this entity type is not supported currently.")
   }
 
   post("/addAccount"){
@@ -39,20 +47,98 @@ class CareerAPIServlet(mongoColl : MongoCollection) extends ScalatraServlet with
     var status=params("status")
     var newObj=MongoDBObject("name"->name, "status" -> status)
     mongoColl+=newObj
-    APIMessage("account",name, "saved")
+    SuccessMessage("account",name, "saved")
+  }
+
+  delete("/deleteAccount/:name"){
+    val q = MongoDBObject("name" -> params("name"))
+    mongoColl.findAndRemove(q) match {
+      case Some(x) => SuccessMessage("project", params("name"), "was deleted")
+      case None => SuccessMessage("project",params("name"),"not found")
+    }
+  }
+
+  //below is a routing to handle errors.
+  error {
+  case e: Throwable => {
+    ErrorMessage("Error happened during handling of account entity", e.toString())
+  }
+}
+
+  def convertDBObjectToAccount(obj: MongoDBObject): Account ={
+    val name = obj.getAs[String]("name").get
+    val status = obj.getAs[String]("status").get
+    Account(name, status)
+  }
+}
+
+class ProjectAPIServlet(mongoColl : MongoCollection) extends ScalatraServlet with NativeJsonSupport {
+
+  // Sets up automatic case class to JSON output serialization
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  // Before every action runs, set the content type to be in JSON format.
+  before() {
+    contentType = formats("json")
+  }
+
+  get("/"){
+    val results=mongoColl.find()
+    val allProjects= ProjectsColl
+    for (x <- results){
+      allProjects.all += convertDBObjectToProject(x)
+    }
+    allProjects.all
+  }
+
+  get("/:name"){
+    val q = MongoDBObject("name" -> params("name"))
+
+    mongoColl.findOne(q) match {
+      case Some(x) => convertDBObjectToProject(x)
+      case None => SuccessMessage("project",params("name"),"not found")
+    }
   }
 
   post("/addProject"){
-    APIMessage("project","","adding this entity type is not supported currently.")
+    var name=params("name")
+    var status=params("status")
+    var clients=params("clients")
+    var team=params("team")
+    var newObj=MongoDBObject("name"->name, "status" -> status, "clients" -> clients, "team" -> team)
+    mongoColl+=newObj
+    SuccessMessage("project",name, "saved")
   }
 
-  // below is a routing to handle errors.
+  delete("/deleteProject/:name"){
+    val q = MongoDBObject("name" -> params("name"))
+    mongoColl.findAndRemove(q) match {
+      case Some(x) => SuccessMessage("project", params("name"), "was deleted")
+      case None => SuccessMessage("project",params("name"),"not found")
+    }
+  }
+
+  //below is a routing to handle errors.
   error {
   case e: Throwable => {
-    redirect("/api")
+    ErrorMessage("Error happened during handling of project entity", e.toString())
   }
-}
+  }
+
+  def convertDBObjectToProject(obj: MongoDBObject): Project ={
+    val name = obj.getAs[String]("name").get
+    val status = obj.getAs[String]("status").get
+    val clients = obj.getAs[String]("clients").get
+    val team = obj.getAs[String]("team").get
+    Project(name, status,clients, team)
+  }
+
 }
 
+
 case class Account(name: Any, status:Any)
-case class APIMessage(entityType: String, entityID: String, entityStatus: String)
+object AccountsColl {var all = ListBuffer[Account]()}
+case class Project(name: Any, status:Any, clients:Any, team: Any)
+object ProjectsColl {var all = ListBuffer[Project]()}
+case class SuccessMessage(entityType: String, entityID: String, entityStatus: String)
+case class ErrorMessage(message: String, details: String = "")
